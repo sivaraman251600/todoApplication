6042c2ca2b1af706772ea269daa8f2f7bc3e9258
K <251600@NTTDATA.COM>
2022-04-14 13:45:17 +0530

    rectify error in localstorage on todo app

--- todoapp/src/components/todo.js
+++ todoapp/src/components/todo.js
@@ -1,71 +1,69 @@
-import React , { useState, useEffect, useRef } from 'react'
-import { v4 as uuidv4 } from 'uuid';
-import './todo.css'
+import React, { useState, useEffect } from "react";
+import { v4 as uuidv4 } from "uuid";
+import "./todo.css";
 
-export function Todo (){
+const getInitalTodos = () => {
+  if (localStorage.getItem("Todo") !== null) {
+    var newTodo = localStorage.getItem("Todo");
+    return JSON.parse(newTodo);
+  } else {
+    return [];
+  }
+};
+export function Todo() {
+  const [inputValue, setInputValue] = useState("");
+  const [todos, setTodos] = useState(() => getInitalTodos());
 
-    const [inputValue , setInputValue] = useState('')
-    const [todos , setTodos] = useState([])
-    const firstRender  = useRef(true);
-    
-    const addTodo = (e) =>{
-        e.preventDefault();
-        
-        if(inputValue.trim() === ''){
-            alert('Please Enter Todo Value')
-            return null
-        }
+  const addTodo = (e) => {
+    e.preventDefault();
 
-        setTodos([
-            ...todos,
-            {
-                text : inputValue,
-                id : uuidv4(),
-            }
-        ])
-
-        setInputValue('')
+    if (inputValue.trim() === "") {
+      alert("Please Enter Todo Value");
+      return null;
     }
 
-    const removeTodo = (id) =>{
-        setTodos(todos.filter((todo)=> todo.id !== id));
-    }
+    setTodos([
+      ...todos,
+      {
+        text: inputValue,
+        id: uuidv4(),
+      },
+    ]);
 
-    useEffect(()=>{
-        if(firstRender.current){
-            console.log('true')
-            firstRender.current = false
-        }
+    setInputValue("");
+  };
 
-        else{
-            console.log('false')
-            localStorage.setItem('Todo' , JSON.stringify([...todos]));
-        }
-    },[todos])
+  const removeTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
 
-    useEffect(()=>{
-        if(localStorage.getItem('Todo') !== null){
-            var newTodo = localStorage.getItem('Todo')
-            setTodos(JSON.parse([...todos ,newTodo]))
-        }
-    },[])
+  useEffect(() => {
+    localStorage.setItem("Todo", JSON.stringify([...todos]));
+  }, [todos]);
 
-    return(
-        <div className='container'>
-            <form onSubmit={addTodo}>
-                <input type={'text'} value={inputValue} placeholder={'Enter Your Todo'} onChange={(e)=>setInputValue(e.target.value)}/>
-                <button type='submit'>Add</button>
-            </form>
-            {
-                todos.map((todo)=>(
-                    <div>
-                    <div key={todo.id} className='todo'>
-                        <p>{todo.text}</p>
-                        <i onClick={()=>removeTodo(todo.id)} className="fa-solid fa-trash-can"></i>
-                    </div><br/>
-                    </div>
-                ))
-            }
+  return (
+    <div className="container">
+      <form onSubmit={addTodo}>
+        <input
+          type={"text"}
+          value={inputValue}
+          placeholder={"Enter Your Todo"}
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+        <button type="submit">Add</button>
+      </form>
+      {todos.map((todo) => (
+        <div>
+          <div key={todo.id} className="todo">
+            <p>{todo.text}</p>
+            <i
+              onClick={() => removeTodo(todo.id)}
+              className="fa-solid fa-trash-can"
+            ></i>
+          </div>
+          <br />
         </div>
-    )
+      ))}
+    </div>
+  );
 }
